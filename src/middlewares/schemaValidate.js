06a48be7a6a449d8371132cd80fe3fda4930b02c
olehhapuk <oleggapuk20@gmail.com>
2021-10-24 00:42:56 +0300

    Init

---
+++ src/middlewares/schemaValidate.js
@@ -0,0 +1,8 @@
+module.exports = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (error) {
+    res.status(422).json(error);
+  }
+};
